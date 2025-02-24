70fa96b9fce8da0722cab720fa5348653f4a84ac
MatiasMadoery <matimadoery@hotmail.com>
2025-02-24 10:28:14 -0300

    Proyect loaded

---
+++ AmoElDiseno/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AmoElDiseno.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
