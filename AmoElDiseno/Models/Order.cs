5a726478da83cd3a71fc145e0073e1aab0284fc8
Ignacio Merlo <nachoamerlo@gmail.com>
2025-03-10 15:57:51 -0300

    DeliveryDate

--- AmoElDiseno/Models/Order.cs
+++ AmoElDiseno/Models/Order.cs
@@ -1,13 +1,30 @@
-﻿namespace AmoElDiseno.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AmoElDiseno.Models
 {
     public class Order
     {
         public int Id { get; set; }
+        [Display(Name = "Número de Pedido")]
         public string? OrderNumber { get; set; }
         public int CustomerId { get; set; }
         public Customer? Customer { get; set; }
+        [Display(Name = "Fecha")]
         public DateTime? Date { get; set; } = DateTime.Now;
-        public string? Details { get; set; }        
+        [Display(Name = "Fecha de Entrega")]
+        public DateTime? DeliveryDate
+        {
+            get
+            {
+                return Date?.AddDays(30);
+            }
+            set
+            {
+            }
+        }
+        [Display(Name = "Detalles")]
+        public string? Details { get; set; }
+        [Display(Name = "Estado")]
         public OrderStatus Status { get; set; }        
         public decimal? Total { get; set; }
         public ICollection<PaymentDelivery>? PaymentDeliveries { get; set; }
--- AmoElDiseno/obj/Debug/net8.0/AmoElDiseno.AssemblyInfo.cs
+++ AmoElDiseno/obj/Debug/net8.0/AmoElDiseno.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmoElDiseno")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d0263a7d592263712e642d8e8cf29d3d9b2b192")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d83fa4738c94c88f783f7e956a2a3e9878beeb99")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmoElDiseno")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmoElDiseno")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
