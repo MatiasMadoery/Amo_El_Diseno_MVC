4e6ed6eb06d4f5501423fe2224c2d1b708ca4d51
Ignacio Merlo <nachoamerlo@gmail.com>
2025-03-12 00:43:17 -0300

    Color changed in relation of dates

--- AmoElDiseno/obj/Debug/net8.0/AmoElDiseno.AssemblyInfo.cs
+++ AmoElDiseno/obj/Debug/net8.0/AmoElDiseno.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmoElDiseno")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d83fa4738c94c88f783f7e956a2a3e9878beeb99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a726478da83cd3a71fc145e0073e1aab0284fc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmoElDiseno")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmoElDiseno")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
