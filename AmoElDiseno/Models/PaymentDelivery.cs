19c4a48de646d2f0a8189d02d1ba1b2aec48f6e1
MatiasMadoery <matimadoery@hotmail.com>
2025-03-19 10:07:22 -0300

    Views errors fixed

--- AmoElDiseno/Models/PaymentDelivery.cs
+++ AmoElDiseno/Models/PaymentDelivery.cs
@@ -1,4 +1,6 @@
-﻿namespace AmoElDiseno.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AmoElDiseno.Models
 {
     public class PaymentDelivery
     {      
@@ -6,6 +8,7 @@
         public int OrderId { get; set; }
         public Order? Order { get; set; }
         public DateTime? Date { get; set; } = DateTime.Now;
+        [Required(ErrorMessage = "El campo Monto es obligatorio!")]     
         public decimal? Amount { get; set; }        
 
     }
