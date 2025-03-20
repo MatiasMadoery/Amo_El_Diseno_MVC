3899876982914d2faceebcf3d68a096aabb473de
MatiasMadoery <matimadoery@hotmail.com>
2025-03-20 13:10:24 -0300

    PaymentDElivery view fixed

--- AmoElDiseno/Models/PaymentDelivery.cs
+++ AmoElDiseno/Models/PaymentDelivery.cs
@@ -7,8 +7,9 @@ namespace AmoElDiseno.Models
         public int Id { get; set; }
         public int OrderId { get; set; }
         public Order? Order { get; set; }
+        [Display(Name = "Fecha")]
         public DateTime? Date { get; set; } = DateTime.Now;
-
+        [Display(Name = "Monto")]
         public decimal? Amount { get; set; } = 0;
 
     }
