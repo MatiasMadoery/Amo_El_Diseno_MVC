c9c7082cb04420b229a7bf28d794a4472ea88153
Ignacio Merlo <nachoamerlo@gmail.com>
2025-03-26 08:32:10 -0300

    DeliveryDate fixed

--- AmoElDiseno/Models/Order.cs
+++ AmoElDiseno/Models/Order.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using AmoElDiseno.Migrations;
+using System.ComponentModel.DataAnnotations;
 
 namespace AmoElDiseno.Models
 {
     public class Order
     {
+        private DateTime? _deliveryDate;
         public int Id { get; set; }
         [Display(Name = "Número de Pedido")]
         public string? OrderNumber { get; set; }
@@ -17,10 +19,11 @@ namespace AmoElDiseno.Models
         {
             get
             {
-                return Date?.AddDays(30);
+                return _deliveryDate ?? Date?.AddDays(30);
             }
             set
             {
+                _deliveryDate = value;
             }
         }
         [Display(Name = "Detalles")]
