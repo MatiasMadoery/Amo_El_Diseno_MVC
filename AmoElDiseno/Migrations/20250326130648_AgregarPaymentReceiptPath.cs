05784ea87b68397a78a8c24abefd3ec64a66b985
MatiasMadoery <matimadoery@hotmail.com>
2025-03-26 10:19:24 -0300

    Add PaymentReceiptPDFPath for Expenses

---
+++ AmoElDiseno/Migrations/20250326130648_AgregarPaymentReceiptPath.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmoElDiseno.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarPaymentReceiptPath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "PaymentDeliveries",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentReceiptPDFPath",
+                table: "Expenses",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentReceiptPDFPath",
+                table: "Expenses");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "PaymentDeliveries",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+        }
+    }
+}
